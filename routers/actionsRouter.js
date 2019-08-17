a352bcb5c7427db02d500945305da0bd50c71174
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-17 12:46:48 -0600

    Added the GET request within the routers/actionsRouter.js file

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const Actions = require("../models/actionsModel");
+
+router.get("/", async (req, res) => {
+  try {
+    const actions = await Actions.find();
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
