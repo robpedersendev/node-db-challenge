2a73f087946a187d4778d16394b017d055b4bb8d
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-17 11:23:33 -0600

    Added the server,js file
    In this file I set up the future usable router paths, the custom logger middlewware, Initialized express and helmet, and also set up the lsiten functionality

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = process.env.PORT || 6500;
+
+const server = express();
+
+server.use(helmet());
+ServiceWorker.use(express.json());
+
+//Logs to the terminal
+server.use(logger);
+
+function logger(req, res, next){
+    console.log(`${req.method} at ${req.url} was just performed`)
+    next();
+};
+
+server.get('/', (req, res) =>{
+    res.status(200).send('<h1>Welcome to the NodeDB Challenge</h1>')
+});
+
+server.use('/api/projects', require(".routers/projectsRouter"));
+server.use('/api/actions', require(".routers/actionsRouter"));
+
+server.listen(PORT, ()=>{
+    console.log(`The server is listening on ${port}`)
+});
