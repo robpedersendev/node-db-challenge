db2a988d12b989fedfb357a2774f2ba058ef4cc6
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-17 14:17:18 -0600

    Corrected spelling in projectsRouter

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const db = require("../db-models/projectsModel-db");
 
 router.get("/", async (req, res) => {
