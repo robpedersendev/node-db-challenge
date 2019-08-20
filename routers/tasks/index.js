7a017007dda9b2f061b7e236c198b4e1d45a9485
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 21:50:54 -0600

    Added the GET for the tasks route

--- routers/tasks/index.js
+++ routers/tasks/index.js
@@ -1,4 +1,27 @@
 const router = require("express").Router();
 const db = require("../../database/config");
 
+router.get("/", async (req, res) => {
+  try {
+    const tasks = (await db
+      .select(
+        "tasks.*",
+        "projects.description as project_description",
+        "projects.name as project_name"
+      )
+      .from("tasks")
+      .leftJoin("projects", "tasks.project_id", "projects.id")).map(task => {
+      return {
+        ...task,
+        completed: task.completed === 0 ? false : true
+      };
+    });
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({
+      error: "Error occurred while attempting to get the tasks"
+    });
+  }
+});
+
 module.exports = router;
