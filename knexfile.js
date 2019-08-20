eadb1fff65c214e746242588de116499615264a6
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 19:32:43 -0600

    Reverted changes to files server.js and to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,11 @@
+// Update with your config settings.
+
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./database/projects.db3"
+      filename: "./data/db.sqlite3"
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations/"
-    },
-    seeds: {
-      directory: "./database/seeds/"
-    }
-  },
-  staging: {},
-  production: {}
+    useNullAsDefault: true
+  }
 };
