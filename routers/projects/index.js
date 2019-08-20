2fbcbf3c036c7c552718831002a7d428461d691b
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 21:45:43 -0600

    Added the GET by ID  for the projects route

--- routers/projects/index.js
+++ routers/projects/index.js
@@ -16,3 +16,29 @@ router.get("/", async (req, res) => {
     });
   }
 });
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    let [project] = await db("projects").where({ id });
+
+    if (!project) {
+      return res.status(404).json({
+        error: `Project with 'id' ${id} does not exist`
+      });
+    } else {
+      const tasks = await db("tasks").where({ project_id: id });
+      const resources = await db("resources").whereIn("id", function() {
+        this.select("resource_id")
+          .from("project_resources")
+          .where({ project_id: id });
+      });
+      project = { ...project, tasks, resources };
+      return res.json(project);
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "Error occurred while attempting to get the project"
+    });
+  }
+});
