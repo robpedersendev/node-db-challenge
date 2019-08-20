f9828e27175872e86496c4b19632a943264b356e
Robert Pedersen <rob84044@gmail.com>
2019-08-20 16:23:21 -0600

    Added comments to the knexfile.js file

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,19 @@
 module.exports = {
+  //Sets up development environment of KNEX
   development: {
+    //What type of DB we are using
     client: "sqlite3",
     connection: {
+      //Where the DB file should be saved to/created at
       filename: "./database/projects.db3"
     },
+    //Replaces undefined keys as a NULL value in the DB
     useNullAsDefault: true,
+    //Where the migrations folder will be saved/created at
     migrations: {
       directory: "./database/migrations/"
     },
+    //Where the seed (the initial data for the DB) data is saved/created at
     seeds: {
       directory: "./database/seeds/"
     }
