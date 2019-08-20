ec9eeeed5d54be11bedff35cbc7410f855290414
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 21:49:01 -0600

    Added the POST for the resources route

--- routers/resources/index.js
+++ routers/resources/index.js
@@ -12,4 +12,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      error: "Resource property `name` is required!"
+    });
+  }
+
+  try {
+    const [id] = await db("resources").insert({ name, description });
+    const [resource] = await db("resources").where({ id });
+    res.status(201).json(resource);
+  } catch (error) {
+    res.status(500).json({
+      error: "Error occurred while attempting to add the resource"
+    });
+  }
+});
+
 module.exports = router;
