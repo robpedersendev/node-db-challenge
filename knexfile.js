bd25117c388d59adf83aa6def3cfd283f676724b
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 19:22:12 -0600

    Restructured the knex file

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/db.sqlite3"
+      filename: "./database/projects.db3"
     },
-    useNullAsDefault: true
-  }
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations/"
+    },
+    seeds: {
+      directory: "./database/seeds/"
+    }
+  },
+  staging: {},
+  production: {}
 };
