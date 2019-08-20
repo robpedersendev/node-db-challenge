82bfb6ebd4bdf0c103699032496b2bbbecd59895
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 21:48:38 -0600

    Added the GET for the resources route

--- routers/resources/index.js
+++ routers/resources/index.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const db = require("../../database/config");
 
+router.get("/", async (req, res) => {
+  try {
+    const resources = await db("resources");
+    res.json(resources);
+  } catch (error) {
+    res.status(500).json({
+      error: "Error occurred while attempting to get the resources"
+    });
+  }
+});
+
 module.exports = router;
