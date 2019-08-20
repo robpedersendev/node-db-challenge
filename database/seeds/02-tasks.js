ceaefacd8613c52852e9aff617353fa1b027bf6c
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-19 21:14:05 -0600

    Added the 02-tasks file

---
+++ database/seeds/02-tasks.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex) {
+  return knex("tasks").insert([
+    {
+      description: "task i description",
+      notes: "these are notes",
+      completed: false,
+      project_id: 1
+    },
+    {
+      description: "task ii description",
+      notes: "these are notes",
+      completed: true,
+      project_id: 2
+    },
+    {
+      description: "task iii description",
+      notes: "these are notes",
+      completed: false,
+      project_id: 3
+    },
+    {
+      description: "task iv description",
+      notes: "these are notes",
+      completed: true,
+      project_id: 4
+    }
+  ]);
+};
