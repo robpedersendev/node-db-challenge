5318ca2c168cf355bcb630632971dbfd28ae3d9d
Robert Pedersen <rob84044@gmail.com>
2019-08-20 16:22:58 -0600

    Added comments to the index.js file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+//Imports the server.js functions
 const server = require("./server");
+//Sets the port to run the server to 5000 or to whatever the .env file is set to
 const PORT = process.env.PORT || 5000;
 
+//This is the initial GET request at the base URL of "/" 
 server.get("/", (req, res) => {
+  //This sends a JSON object back
   res.json({
     message:
       "Thanks for trying this API! CRUD operations can be made on the " +
@@ -14,6 +18,7 @@ server.get("/", (req, res) => {
   });
 });
 
+//Tell the server what port to listen to and also to print to the console
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
