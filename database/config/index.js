db7f0201f0c85c464a9a53d772797d75f16ba820
Robert Pedersen <rob84044@gmail.com>
2019-08-21 15:34:00 -0600

    Added comments to the database/config/index.js file

--- database/config/index.js
+++ database/config/index.js
@@ -2,3 +2,6 @@ const knex = require("knex");
 const config = require("../../knexfile");
 
 module.exports = knex(config.development);
+
+
+//This file is needed so that anytime KNEX is called to do a SQL query on the database, KNEX knows where to go - how to connect - and any other relevant information. 
