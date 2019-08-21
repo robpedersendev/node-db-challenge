7689f825eff8303f5bf873f705debc2f58c7ce18
Robert Pedersen <rob84044@gmail.com>
2019-08-21 15:42:29 -0600

    Added comments to the database/migrations/index.js file

--- database/migrations/20190819205306_create-table.js
+++ database/migrations/20190819205306_create-table.js
@@ -1,6 +1,15 @@
+//Once a migration file has been created, after you run npx knex migrate:latest, you will need to create a new one if your knex seed files require an additional table within your existing database. 
+
+//This doesnt mean that you can not create a new one directly in SQLite or other SQL platform, however. 
+
+//The exports.up is the instructions to create or add data to the db
 exports.up = function(knex) {
+  //Create the Schema
   return knex.schema
+    //.createTable creates the table and takes in two arguments
+    //First is the name of the table, second is the individual column type, then the column name, and the additional attributes
     .createTable("projects", table => {
+      //This defaults to the name of "id" in the database
       table.increments();
       table.string("name").notNullable();
       table.string("description");
@@ -50,6 +59,7 @@ exports.up = function(knex) {
     });
 };
 
+//The exports.down is the instructions to delete the database tables, but you have to call them explicitly
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists("project_resources")
